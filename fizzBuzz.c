433ffbfe733f15f555d9a0e9884365816a9f0a38
VoightG <100247267+VoightG@users.noreply.github.com>
2022-11-19 16:57:10 -0600

    Create fizzBuzz.c

---
+++ fizzBuzz.c
@@ -0,0 +1,16 @@
+/*Below is the code for the solution to the classic FizzBuzz question.*/
+#include <stdio.h>
+
+int main(){
+  for(int i = 1; i <= 100; i++){
+    if(i%15==0){
+      printf("FizzBuzz\n");
+    } else if(i%5==0){
+      printf("Buzz\n");
+    } else if(i%3==0){
+      printf("Fizz\n");
+    } else{
+    printf("%d\n", i);
+    }
+  }
+}
